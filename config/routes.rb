1b94ec123be03c9103ea6656a4c3148c5e94c9a2
Jocelyn Proctor <jocelynkproctor@gmail.com>
2020-06-03 14:23:58 +0200

    created components page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'pages#components'
   devise_for :users
 
   resources :consultations, only: [:new, :create, :edit, :update, :show]
