d6d1488f83acf799dc136aa4f36e519abe3686ae
Victoria Gonzales <vagp89@gmail.com>
2020-06-03 12:18:45 +0200

    consultations controller fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'clean db'
+Consultation.destroy_all
+User.destroy_all
+puts 'db is now clean'
+
+user_one = User.create(username:'luis', email:"luis@example", first_name:"Luis", encrypted_password:"servicio")
+user_two = User.create(username:'carlos', email:"car@example", first_name:"carlos", encrypted_password:"servicio89")
+
+puts 'is created'
+
+
