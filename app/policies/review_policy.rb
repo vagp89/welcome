8d840cf0c410a5f36432f7a26de6b3e5929b6edc
Sanja Wetzel <sanjwich@gmail.com>
2020-06-03 18:29:55 +0200

    akdfja

---
+++ app/policies/review_policy.rb
@@ -0,0 +1,19 @@
+class ReviewPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    new?
+  end
+
+  def show?
+    true
+  end
+end
