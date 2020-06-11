f78cba5c54211637f1667fae2363331596fed571
Victoria Gonzales <vagp89@gmail.com>
2020-06-11 12:30:19 +0200

    chatroom fixes

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -2,11 +2,15 @@ import consumer from "./consumer";
 
 const initChatroomCable = () => {
   const messagesContainer = document.getElementById('messages');
+  const inputField = document.querySelector('#message_content');
   if (messagesContainer) {
     const id = messagesContainer.dataset.chatroomId;
     consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
       received(data) {
         messagesContainer.insertAdjacentHTML('beforeend', data);
+        location.reload();
+        inputField.value = '';
+        window.scrollTo(0, document.body.scrollHeight);
       }
     });
 
